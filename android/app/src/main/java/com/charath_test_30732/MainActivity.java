735f8c6f8ae67f20309dc67ba4809ad7363dbab5
Crowdbotics <team@crowdbotics.com>
2021-09-22 00:59:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/charath_test_30732/MainActivity.java
@@ -0,0 +1,15 @@
+package com.charath_test_30732;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "charath_test_30732";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "charath_test_30732Identifier",
+  url: "https://charath_test_30732Identifier.botics.co",
+  api: "https://charath_test_30732Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
